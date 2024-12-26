14a82824d10fa45ba294bc1350875c24e444e195
Navonil Majumder <navonil@David-8.local>
2024-12-27 02:46:42 +0800

    keymap: del -> caps_lock

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,
 	MT(MOD_LCTL, KC_ESC),	LCTL_T(KC_A),   	LSFT_T(KC_S),   	LALT_T(KC_D),  		LWIN_T(KC_F),   	KC_G,   	RWIN_T(KC_H),   	RALT_T(KC_J),   	RSFT_T(KC_K),   	RCTL_T(KC_L),  		KC_SCLN,	KC_QUOT, 	            KC_ENT,
-	KC_LSFT,	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_ESC,
+	KC_LSFT,	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_CAPS,
 	KC_LCTL,	KC_LALT,	KC_LGUI,										LT(7, KC_SPC),   					KC_RGUI,	MO(1),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer 1 Mac fn
