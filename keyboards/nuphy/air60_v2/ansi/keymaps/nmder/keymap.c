9943340fb93661b34c83fcea0e05ce9585e84297
Navonil Majumder <n.majumder.2009@gmail.com>
2025-02-02 14:14:29 +0800

    Hold caps lock for media keys

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,
-	MT(MOD_LCTL, KC_ESC),	LCTL_T(KC_A),   	LSFT_T(KC_S),   	LALT_T(KC_D),  		LWIN_T(KC_F),   	KC_G,   	RWIN_T(KC_H),   	RALT_T(KC_J),   	RSFT_T(KC_K),   	RCTL_T(KC_L),  		KC_SCLN,	KC_QUOT, 	            KC_ENT,
+	LT(1, KC_ESC),	LCTL_T(KC_A),   	LSFT_T(KC_S),   	LALT_T(KC_D),  		LWIN_T(KC_F),   	KC_G,   	RWIN_T(KC_H),   	RALT_T(KC_J),   	RSFT_T(KC_K),   	RCTL_T(KC_L),  		KC_SCLN,	KC_QUOT, 	            KC_ENT,
 	KC_LSFT,	KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_CAPS,
 	KC_GLB, 	KC_LALT,	KC_LGUI,										LT(7, KC_SPC),   					KC_RGUI,	MO(1),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
 
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [7] = LAYOUT(
 	_______, 	KC_F1,  	KC_F2,  	KC_F3,   	KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9,   	KC_F10, 	KC_F11, 	KC_F12, 	KC_DEL,
 	_______, 	_______,  	S(A(KC_W)),	S(A(KC_E)),	_______,   	_______,   	S(A(KC_Y)),	S(A(KC_U)),	S(A(KC_I)),	S(A(KC_O)),	_______,   	_______,	_______,    _______,
-	MO(1),   	_______,   	_______,   	_______,  	_______,   	S(A(KC_G)),	KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	KC_BSPC,   	_______,	            _______,
+	_______,   	_______,   	_______,   	_______,  	_______,   	S(A(KC_G)),	KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	KC_BSPC,   	_______,	            _______,
 	NM_MOD0,	_______,   	_______,   	_______,  	S(A(KC_V)),	S(A(KC_B)), KC_ENT,   	S(A(KC_S)),	_______,   	KC_DEL,  	KC_BSLS,	KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2,
 	_______,	_______,	_______,										_______, 							_______,	_______,   	KC_MS_L,	KC_MS_D,    KC_MS_R)
 };
