12fcd581e7bc15cf9b41c941a93339fa2b8390df
Navonil Majumder <n.majumder.2009@gmail.com>
2025-02-11 01:07:51 +0800

    sft + lsft_t(bc_bspc) -> kc_del

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -66,10 +66,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t delete_key_override2 = ko_make_basic(MOD_MASK_SHIFT, LSFT_T(KC_BSPC), KC_DEL);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
     &delete_key_override,
+    &delete_key_override2,
     NULL
 };
 
