c8e3e48f02eb7a941083c950ca6faab68961a9d9
Navonil Majumder <n.majumder.2009@gmail.com>
2025-03-22 15:44:58 +0800

    TAB is moved to . in spc layer from f in spc and num layers

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -226,15 +226,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [6] = LAYOUT(
 	_______, 	KC_F1,  	KC_F2,  	KC_F3,   	KC_F4,  		KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9,   	KC_F10, 	KC_F11, 	KC_F12, 	KC_DEL,
 	_______, 	KC_EXLM,  	KC_UNDS,	KC_EQUAL,	KC_GRV,   		KC_TILD,   	KC_ASTR,	KC_CIRC,	KC_DLR,		KC_PERC,	KC_PLUS,   	_______,	_______,    _______,
-	_______,   	_______,   	_______,   	_______,  	LWIN_T(KC_TAB),	KC_AT,		KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	KC_BSPC,   	_______,	            _______,
-	_______,	KC_AMPR,   	KC_LCBR,   	KC_RCBR,  	KC_LPRN,		KC_RPRN, 	KC_ENT,   	KC_MINUS,	KC_HASH,   	_______,  	KC_BSLS,	KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2,
+	_______,   	_______,   	_______,   	_______,  	_______,		KC_AT,		KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	KC_BSPC,   	_______,	            _______,
+	_______,	KC_AMPR,   	KC_LCBR,   	KC_RCBR,  	KC_LPRN,		KC_RPRN, 	KC_ENT,   	KC_MINUS,	KC_HASH,   	KC_TAB,  	KC_BSLS,	KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2,
 	_______,	_______,	_______,											_______, 							_______,	_______,   	KC_MS_L,	KC_MS_D,    KC_MS_R),
 
 // layer 8 numbers
 [7] = LAYOUT(
 	KC_ESC, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	MAC_SEARCH, 	MAC_VOICE,  MAC_DND,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	_______,
 	_______, 	KC_1,	    KC_2,	    KC_3,	    KC_4,			KC_5,		KC_6,		KC_7,   	KC_8,   	KC_9,		KC_0,   	_______,	_______,    _______,
-	KC_BSPC,   	_______,   	_______,   	_______,  	LWIN_T(KC_TAB),	_______,	KC_H,		KC_J,		KC_K,		KC_L,		KC_BSPC,   	_______,	            _______,
+	KC_BSPC,   	_______,   	_______,   	_______,  	_______,		_______,	KC_H,		KC_J,		KC_K,		KC_L,		KC_BSPC,   	_______,	            _______,
 	_______,	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_VOLD,		KC_VOLU, 	KC_LBRC,	KC_RBRC,	_______,	_______,	_______,	_______, 	_______, 	_______,
 	_______,	_______,	_______,											_______, 							_______,	_______,   	_______,	_______,    _______)
 };
