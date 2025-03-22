ad86faee19dadb6a93973a69f4efeeff49cd67d5
Navonil Majumder <n.majumder.2009@gmail.com>
2025-03-22 11:55:33 +0800

    Lower COMBO_TERM for i,o

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -83,6 +83,8 @@ combo_t key_combos[] = {
 #ifdef COMBO_TERM_PER_COMBO
 uint16_t get_combo_term(uint16_t combo_index, combo_t *combo) {
     switch (combo_index) {
+        case 1:
+            return 10;
         case 5:
             return 30;
         case 15:
