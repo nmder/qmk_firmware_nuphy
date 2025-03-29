1c021e24cb048ce3e97fb09bf94727653a83a43d
Navonil Majumder <n.majumder.2009@gmail.com>
2025-03-29 14:14:35 +0800

    num layer: asdfg -> 67890

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -230,9 +230,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // layer 8 numbers
 [7] = LAYOUT(
-	KC_ESC, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	MAC_SEARCH, 	MAC_VOICE,  MAC_DND,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	_______,
-	_______, 	KC_1,	    KC_2,	    KC_3,	    KC_4,			KC_5,		KC_6,		KC_7,   	KC_8,   	KC_9,		KC_0,   	_______,	_______,    _______,
-	KC_BSPC,   	_______,   	_______,   	_______,  	_______,		_______,	KC_H,		KC_J,		KC_K,		KC_L,		KC_BSPC,   	_______,	            _______,
+	KC_ESC, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	MAC_SEARCH, MAC_VOICE,  MAC_DND,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	_______,
+	_______, 	KC_1,	    KC_2,	    KC_3,	    KC_4,		KC_5,		KC_6,		KC_7,   	KC_8,   	KC_9,		KC_0,   	_______,	_______,    _______,
+	KC_BSPC,   	LCTL_T(KC_6),  	LSFT_T(KC_7),  	LALT_T(KC_8),	LWIN_T(KC_9),	KC_0,		KC_H,		KC_J,		KC_K,		KC_L,		KC_BSPC,   	_______,	            _______,
 	_______,	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_VOLD,		KC_VOLU, 	KC_LBRC,	KC_RBRC,	_______,	_______,	_______,	_______, 	_______, 	_______,
 	_______,	_______,	_______,											_______, 							_______,	_______,   	_______,	_______,    _______)
 };
