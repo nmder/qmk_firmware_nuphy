8bb9a87fd10802ede5aca82ab94005867ab8c511
Navonil Majumder <n.majumder.2009@gmail.com>
2025-05-26 14:49:04 +0800

    Rearrangement of `num` layer and media keys

--- keyboards/shortcut/bridge75_nolight/keymaps/nmder/keymap.c
+++ keyboards/shortcut/bridge75_nolight/keymaps/nmder/keymap.c
@@ -22,21 +22,21 @@ const key_override_t *key_overrides[] = {
     &delete_key_override3,
 };
 
-const uint16_t PROGMEM combo_lbrc[] = {RALT_T(KC_J), KC_I, COMBO_END};
+const uint16_t PROGMEM combo_lbrc[] = {RWIN_T(KC_J), KC_I, COMBO_END};
 const uint16_t PROGMEM combo_rbrc[] = {KC_U, KC_I, COMBO_END};
-const uint16_t PROGMEM combo_plus[] = {KC_M, KC_COMM, COMBO_END};
-const uint16_t PROGMEM combo_mins[] = {KC_N, KC_M, COMBO_END};
-const uint16_t PROGMEM combo_tab_[] = {RCTL_T(KC_L), RSFT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM combo_plus[] = {KC_DOT, KC_COMM, COMBO_END};
+const uint16_t PROGMEM combo_mins[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM combo_tab_[] = {RSFT_T(KC_L), RALT_T(KC_K), COMBO_END};
 const uint16_t PROGMEM combo_hash[] = {LWIN_T(KC_F), RWIN_T(KC_H), COMBO_END};
 const uint16_t PROGMEM combo_uscr[] = {LSFT_T(KC_S), LALT_T(KC_D), COMBO_END};
 const uint16_t PROGMEM combo_equl[] = {LALT_T(KC_D), LWIN_T(KC_F), COMBO_END};
 const uint16_t PROGMEM combo_ampr[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM combo_cart[] = {LWIN_T(KC_F), KC_E, COMBO_END};
-const uint16_t PROGMEM combo_dolr[] = {RWIN_T(KC_H), KC_U, COMBO_END};
-const uint16_t PROGMEM combo_lprn[] = {RWIN_T(KC_H), RALT_T(KC_J), COMBO_END};
-const uint16_t PROGMEM combo_rprn[] = {RALT_T(KC_J), RSFT_T(KC_K), COMBO_END};
+const uint16_t PROGMEM combo_dolr[] = {KC_H, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_lprn[] = {KC_H, RWIN_T(KC_J), COMBO_END};
+const uint16_t PROGMEM combo_rprn[] = {RWIN_T(KC_J), RALT_T(KC_K), COMBO_END};
 const uint16_t PROGMEM combo_perc[] = {LALT_T(KC_D), KC_C, COMBO_END};
-const uint16_t PROGMEM combo_astr[] = {RALT_T(KC_J), KC_N, COMBO_END};
+const uint16_t PROGMEM combo_astr[] = {RALT_T(KC_K), KC_M, COMBO_END};
 const uint16_t PROGMEM combo_atra[] = {RALT_T(KC_J), LALT_T(KC_D), COMBO_END};
 const uint16_t PROGMEM combo_baqt[] = {LALT_T(KC_D), KC_V, COMBO_END};
 const uint16_t PROGMEM combo_tild[] = {RALT_T(KC_J), KC_B, COMBO_END};
@@ -104,7 +104,6 @@ bool get_combo_must_tap(uint16_t combo_index, combo_t *combo) {
 #endif
 
 enum hjkl {
-    ID_H,
     ID_J,
     ID_K,
     ID_L,
@@ -115,8 +114,8 @@ enum hjkl {
     ID_OUT
 };
 
-const uint16_t mt_taps[] = { KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_AT, KC_AMPR, KC_DQUO };
-const uint16_t mt_mods[] = { KC_RWIN, KC_RALT, KC_RSFT, KC_RCTL, KC_LCTL, KC_LSFT, KC_LWIN };
+const uint16_t mt_taps[] = { KC_LCBR, KC_RCBR, KC_RPRN, KC_AT, KC_AMPR, KC_DQUO };
+const uint16_t mt_mods[] = { KC_RWIN, KC_RALT, KC_RSFT, KC_LCTL, KC_LSFT, KC_LWIN };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
@@ -125,10 +124,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     enum hjkl kc;
     switch (keycode) {
-        case KC_H:
-            kc = ID_H;
-            break;
-
         case KC_J:
             kc = ID_J;
             break;
@@ -174,7 +169,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
         }
     } else {
-        for (enum hjkl id = ID_H; id < L_ID; id++) {
+        for (enum hjkl id = ID_J; id < L_ID; id++) {
             if (tap_timer[id] != 0 && (record->event.pressed || timer_elapsed(tap_timer[id]) > TAPPING_TERM)) {
                 tap_timer[id] = 0;
                 register_code(mt_mods[id]);
@@ -190,7 +185,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, KC_BRID, KC_BRIU, KC_MCTL,	KC_LPAD, KC_F5,	  KC_F6,   KC_MPRV,	KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,	KC_VOLU,                   KC_DEL,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        LT(NUM, KC_ESC), LCTL_T(KC_A),       LSFT_T(KC_S),       LALT_T(KC_D),       LWIN_T(KC_F),       KC_G,       RWIN_T(KC_H),       RALT_T(KC_J),       RSFT_T(KC_K),       RCTL_T(KC_L),    LT(NUM, KC_SCLN), KC_QUOT,          KC_ENT,           KC_PGDN,
+        LT(NUM, KC_ESC), LCTL_T(KC_A),       LSFT_T(KC_S),       LALT_T(KC_D),       LWIN_T(KC_F),       KC_G,       KC_H,       RWIN_T(KC_J),       RALT_T(KC_K),       RSFT_T(KC_L),    RCTL_T(KC_SCLN), LT(NUM, KC_QUOT),          KC_ENT,           KC_PGDN,
         LSFT_T(KC_BSPC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                   KC_RSFT, KC_UP,   KC_END,
         KC_GLB, KC_LALT, KC_LGUI,                            LT(MAC_B, KC_SPC),                  KC_RGUI, MO(WIN_FN),       KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -207,27 +202,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_B] = LAYOUT_ansi(
         KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                   _______,
         _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL,          _______,
-        _______, 	KC_EXLM,  	KC_UNDS,	KC_EQUAL,	KC_GRV,   	KC_TILD,   	KC_ASTR,	KC_CIRC,	KC_DLR,		KC_PERC,	KC_PLUS,   	_______,	_______,    _______, _______,
-        _______,		KC_A,	 	KC_S,		LALT_T(KC_QUOT),	KC_F,   	KC_AT,		KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	LT(FUNC, KC_BSPC),   	_______,	            _______, _______,
-        _______,	KC_AMPR,   	KC_LT,   	KC_GT,  	KC_BSLS,	KC_PIPE, 	KC_ENT,   	KC_MINUS,	KC_HASH,   	KC_TAB,  	KC_BSLS,	_______, _______,    _______,
+        KC_VOLU, 	KC_EXLM,  	KC_UNDS,	KC_EQUAL,	KC_GRV,   	KC_TILD,   	KC_ASTR,	KC_CIRC,	KC_DLR,		KC_PERC,	KC_PLUS,   	_______,	_______,    _______, _______,
+        KC_VOLD,		KC_A,	 	KC_S,		LALT_T(KC_QUOT),	KC_F,   	KC_AT,		KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	KC_BSPC,   	LT(FUNC, KC_QUOT),	            _______, _______,
+        KC_MUTE,	KC_MPRV,   	KC_MPLY,   	KC_MNXT,  	KC_BSLS,	KC_PIPE, 	KC_ENT,   	KC_MINUS,	KC_HASH,   	KC_TAB,  	KC_BSLS,	_______, _______,    _______,
         _______, _______, _______,                             _______,                           _______, _______,          _______, _______, _______
     ),
 
     [NUM] = LAYOUT_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_AMPR,		KC_DQUO,   	KC_QUOT,   	KC_AT,		KC_QUES,    _______, _______, _______,          _______,
-        KC_BSPC, LCTL_T(KC_6),       LSFT_T(KC_7),       LALT_T(KC_8),       LWIN_T(KC_9), KC_0,	KC_H,	KC_J,	KC_K,	KC_L,	KC_BSPC, _______,          _______,          _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU, KC_LBRC,	KC_RBRC,	_______, _______, _______,                   _______, _______, _______,
+        _______, KC_MINUS,    KC_1,    KC_2,    KC_3,    KC_PLUS,    KC_AMPR,		KC_DQUO,   	KC_QUOT,   	KC_AT,		KC_QUES,    _______, _______, _______,          _______,
+        KC_BSPC, LCTL_T(KC_BSPC),       LSFT_T(KC_4),       LALT_T(KC_5),       LWIN_T(KC_6), KC_0,	KC_LPRN,	KC_J,	KC_K,	KC_L,	RCTL_T(KC_BSPC), _______,          _______,          _______,
+        KC_SLSH, KC_ASTR, KC_7, KC_8, KC_9, KC_DOT, KC_LBRC,	KC_RBRC,	_______, _______, _______,                   _______, _______, _______,
         _______, _______, _______,                             MO(FUNC),                           _______, _______,          _______, _______, _______
     ),
 
     [FUNC] = LAYOUT_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,	_______, _______, _______, _______, _______, _______,          _______,
-        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, _______,	_______, _______, _______, _______,          _______,          _______,
-        _______, KC_F11, KC_F12, _______, _______, _______,   _______, _______,	_______, _______, _______,                   _______, _______, _______,
+        _______, _______,   KC_F1,   KC_F2,   KC_F3,   KC_F10,   _______, _______,	_______, _______, _______, _______, _______, _______,          _______,
+        _______, _______,   KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, _______,	_______, _______, _______, _______,          _______,          _______,
+        _______, _______, KC_F7, KC_F8, KC_F9, KC_F12,   _______, _______,	_______, _______, _______,                   _______, _______, _______,
         _______, _______, _______,                             _______,                           _______, _______,          _______, _______, _______
     )
 };
