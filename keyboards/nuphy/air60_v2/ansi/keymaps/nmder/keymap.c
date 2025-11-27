f6e64ece16dcb5cd57921506a08c70c881d107ba
tot <n.majumder.2009@gmail.com>
2025-11-27 21:44:15 +0800

    Sync with zmk and kanata

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -261,7 +261,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______, 	KC_F1,  	KC_F2,  	KC_F3,   	KC_F4,  		KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9,   	KC_F10, 	KC_F11, 	KC_F12, 	KC_DEL,
 	KC_MUTE, 	KC_EXLM,  	KC_UNDS,	KC_EQUAL,	KC_GRV,   		KC_TILD,   	KC_ASTR,	KC_CIRC,	KC_DLR,		KC_PERC,	KC_PLUS,   	_______,	_______,    _______,
 	KC_VOLU,	KC_A,	 	KC_S,		LALT_T(KC_QUOT),	KC_F,	KC_HASH,		KC_LEFT,   	KC_DOWN,   	KC_UP,   	KC_RGHT,  	LT(8, KC_BSPC),   	_______,	            _______,
-	KC_VOLD,	_______,   	KC_MPRV,   	KC_MPLY,  	KC_MNXT,		KC_PIPE, 	KC_ENT,   	KC_MINUS,	KC_HASH,   	KC_TAB,  	KC_BSLS,	KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2,
+	KC_VOLD,	_______,   	KC_MPRV,   	KC_MPLY,  	KC_MNXT,		KC_PIPE, 	KC_ENT,   	KC_MINUS,	KC_PLUS,   	KC_TAB,  	KC_BSLS,	KC_MS_BTN1, KC_MS_U,    KC_MS_BTN2,
 	_______,	_______,	_______,											_______, 							_______,	_______,   	KC_MS_L,	KC_MS_D,    KC_MS_R),
 
 // layer 8 numbers
@@ -269,7 +269,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_ESC, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	MAC_SEARCH, MAC_VOICE,  MAC_DND,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	_______,
 	_______, 	KC_MINUS,	    KC_1,	    KC_2,	    KC_3,		KC_PLUS,		KC_VOLD,		KC_MPRV,   	KC_MPLY,  	KC_MNXT,	KC_VOLU,   	_______,	_______,    _______,
 	_______,   	LCTL_T(KC_BSPC),  	LSFT_T(KC_4),  	LALT_T(KC_5),	LWIN_T(KC_6),	KC_0,		KC_LPRN,		KC_J,		KC_K,		KC_L,		RCTL_T(KC_BSPC),   	_______,	            _______,
-	KC_SLSH,	KC_ASTR,  	KC_7,  	KC_8, 	KC_9,		KC_DOT, 	KC_LBRC,	KC_RBRC,	_______,	_______,	_______,	_______, 	_______, 	_______,
+	KC_SLSH,	KC_ASTR,  	KC_7,  	KC_8, 	KC_9,		KC_DOT, 	_______,	KC_LBRC,	KC_RBRC,	_______,	_______,	_______, 	_______, 	_______,
 	_______,	_______,	_______,											MO(8), 							_______,	_______,   	_______,	_______,    _______),
 
 // layer 9 numbers
