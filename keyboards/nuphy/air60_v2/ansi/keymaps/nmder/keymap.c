8480cfeaf0fa5990841dd9d9e98928c4eb51b717
Navonil Majumder <n.majumder.2009@gmail.com>
2026-03-13 13:29:31 +0800

    Update the combos

--- keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/nmder/keymap.c
@@ -47,15 +47,17 @@ const uint16_t PROGMEM combo_plus[] = {KC_DOT, KC_COMM, COMBO_END};
 const uint16_t PROGMEM combo_mins[] = {KC_COMM, KC_M, COMBO_END};
 const uint16_t PROGMEM combo_tab_[] = {RSFT_T(KC_L), RALT_T(KC_K), COMBO_END};
 const uint16_t PROGMEM combo_uscr[] = {LSFT_T(KC_S), LALT_T(KC_D), COMBO_END};
-const uint16_t PROGMEM combo_equl[] = {LALT_T(KC_D), LWIN_T(KC_F), COMBO_END};
+const uint16_t PROGMEM combo_equl[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM combo_lprn[] = {KC_H, RALT_T(KC_K), COMBO_END};
 const uint16_t PROGMEM combo_rprn[] = {RWIN_T(KC_J), RALT_T(KC_K), COMBO_END};
 const uint16_t PROGMEM combo_astr[] = {RALT_T(KC_K), KC_M, COMBO_END};
-const uint16_t PROGMEM combo_baqt[] = {LALT_T(KC_D), KC_V, COMBO_END};
+const uint16_t PROGMEM combo_tild[] = {LALT_T(KC_D), KC_G, COMBO_END};
 const uint16_t PROGMEM combo_lcrb[] = {KC_H, RSFT_T(KC_L), COMBO_END};
 const uint16_t PROGMEM combo_rcrb[] = {RWIN_T(KC_J), RSFT_T(KC_L), COMBO_END};
-const uint16_t PROGMEM combo_dquo[] = {LSFT_T(KC_S), LWIN_T(KC_F), COMBO_END};
-const uint16_t PROGMEM combo_squo[] = {LWIN_T(KC_F), LCTL_T(KC_A), COMBO_END};
+const uint16_t PROGMEM combo_dquo[] = {LALT_T(KC_D), LWIN_T(KC_F), COMBO_END};
+const uint16_t PROGMEM combo_squo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_hash[] = {LSFT_T(KC_S), LWIN_T(KC_F), COMBO_END};
+const uint16_t PROGMEM combo_bacq[] = {KC_C, KC_X, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_lbrc, KC_LBRC),
@@ -68,11 +70,13 @@ combo_t key_combos[] = {
     COMBO(combo_lprn, KC_LPRN),
     COMBO(combo_rprn, KC_RPRN),
     COMBO(combo_astr, KC_ASTR),
-    COMBO(combo_baqt, KC_GRV),
+    COMBO(combo_tild, KC_TILD),
     COMBO(combo_lcrb, KC_LCBR),
     COMBO(combo_rcrb, KC_RCBR),
     COMBO(combo_dquo, KC_DQUO),
     COMBO(combo_squo, KC_QUOT),
+    COMBO(combo_hash, KC_DQUO),
+    COMBO(combo_bacq, KC_GRV),
 };
 
 #ifdef COMBO_TERM_PER_COMBO
